4fb72dd6c864209fdd90743d05d5f121d49049dd
Malu <marialz.dev@gmail.com>
2025-10-26 22:18:24 -0300

    Atividade 5

---
+++ Atividade 5/dist/cliente.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// 1) Classe Cliente
+// Crie uma classe Cliente com os seguintes atributos:
+// • id: Identificador único do cliente (número);
+// • nome: Nome completo do cliente (string);
+// • cpf: CPF único do cliente (string);
+// • dataNascimento: Data de nascimento do cliente (Date);
+// • contas: Array de contas associadas ao cliente.
---
+++ Atividade 5/src/cliente.ts
@@ -0,0 +1,7 @@
+// 1) Classe Cliente
+// Crie uma classe Cliente com os seguintes atributos:
+// • id: Identificador único do cliente (número);
+// • nome: Nome completo do cliente (string);
+// • cpf: CPF único do cliente (string);
+// • dataNascimento: Data de nascimento do cliente (Date);
+// • contas: Array de contas associadas ao cliente.
